70273ac95ea15ed76e47f3c521cc93c45027e42a
Wojciech Klupś <wojciech.klups@gmail.com>
2019-05-11 09:57:52 +0200

    Added division and teacher models

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,4 +1,59 @@
 package model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int student_id;
+
+    private String name;
+    private String lastname;
+    private int age;
+
+    public Student(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public Student() {
+    }
+
+    public int getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
