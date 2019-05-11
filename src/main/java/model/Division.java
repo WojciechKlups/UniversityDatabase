e7a9b0284550882bf53e6960468b6c941c6a6de8
Wojciech Klupś <wojciech.klups@gmail.com>
2019-05-11 13:13:08 +0200

    Added Course class and implementation of others

--- src/main/java/model/Division.java
+++ src/main/java/model/Division.java
@@ -1,4 +1,32 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Division {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int div_id;
+
+    private String div_name;
+
+    public int getDiv_id() {
+        return div_id;
+    }
+
+    public void setDiv_id(int div_id) {
+        this.div_id = div_id;
+    }
+
+    public String getDiv_name() {
+        return div_name;
+    }
+
+    public void setDiv_name(String div_name) {
+        this.div_name = div_name;
+    }
 }
--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,4 +1,50 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int teacher_id;
+
+    private String name;
+    private String lastname;
+    private int age;
+
+    @ManyToOne
+    private Division division;
+
+    public int getTeacher_id() {
+        return teacher_id;
+    }
+
+    public void setTeacher_id(int teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
